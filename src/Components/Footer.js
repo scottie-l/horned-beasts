f31d30967b2d29bc101115b315a55c0d4abba7f6
Scott Lease <scottie_lease@hotmail.com>
2021-10-27 15:34:27 -0700

    completed feature tasks

---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+import {Component} from 'react';
+import '/home/scottie_l/projects/code301/lab_01/src/CSS/footer.css';
+import Container from 'react-bootstrap/Container';
+
+class Footer extends Component {
+    render() {
+        return(
+            <Container>
+                <>
+                <p>created by: Scott Lease</p>
+                </>
+            </Container>
+        )
+    }
+}
+
+export default Footer;
