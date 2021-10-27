bc67763fe864e65bfa349c1ee6a0839bc6b35649
Scott Lease <scottie_lease@hotmail.com>
2021-10-27 16:04:26 -0700

    fixing some code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './CSS/index.css';
+import './css/index.css';
 import App from './Components/App.js';
 
 ReactDOM.render(
